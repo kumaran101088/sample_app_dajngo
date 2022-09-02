25017291091a71c44055fc9ed835b84ffab5fded
Kumaran <kumaran101088@gmail.com>
2022-09-02 16:25:49 +0530

    20th commit

--- sample_app/sample_app/settings.py
+++ sample_app/sample_app/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
        'NAME': 'member_db',
        'USER': 'postgres',
        'PASSWORD': 'password',
-       'HOST': '34.70.165.125',
-    #    'HOST': '/cloudsql/sample-groi:us-central1:sample-db',
+    #    'HOST': '34.70.165.125',
+       'HOST': '/cloudsql/sample-groi:us-central1:sample-db',
     }
 }
 
