8772dc9550bb6e4d2b1b1c9f38b3e499022629ab
Kumaran <kumaran101088@gmail.com>
2022-09-02 15:56:17 +0530

    18th commit

--- sample_app/myapp/views.py
+++ sample_app/myapp/views.py
@@ -5,6 +5,29 @@ from .models import Member
 # Create your views here.
 
 def home(request):
-    return render(request, 'html_files/index.html')
+    members = Member.objects.all()
+    context = {
+        'count' : members.count()
+    }
+    # df = pd.read_csv(r'C:\Users\Kumaran\Desktop\MOCK_DATA.csv')
+    # for row in range(1, 10000):
+    #     member = Member(
+    #         member_id = df.loc[row, 'id']+1,
+    #         age = df.loc[row, 'age_new'],
+    #         gender = df.loc[row, 'gender'],
+    #         height = df.loc[row, 'height_new'],
+    #         weight = df.loc[row, 'weight_new'],
+    #         st_bp = df.loc[row, 'ap_hi_new'],
+    #         dy_bp = df.loc[row, 'ap_lo_new'],
+    #         chol = df.loc[row, 'cholesterol'],
+    #         gluc = df.loc[row, 'gluc'],
+    #         smoke = df.loc[row, 'smoke'],
+    #         alco = df.loc[row, 'alco'],
+    #         active = df.loc[row, 'active'],
+    #         cardio = df.loc[row, 'cardio'],
+    #         medication = df.loc[row, 'medication_new']
+    #     )
+    #     member.save()
+    return render(request, 'html_files/index.html', context)
 
 
