d6d6f463febc007a26ea7a6fea06fd5afba0a313
Kumaran <kumaran101088@gmail.com>
2022-09-02 13:59:58 +0530

    16th commit

--- sample_app/sample_app/settings.py
+++ sample_app/sample_app/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'sample_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'member_db',
+       'USER': 'postgres',
+       'PASSWORD': 'password',
+       'HOST': '34.70.165.125',
     }
 }
 
