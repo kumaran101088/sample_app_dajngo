d1150cd4758224b4e7943bc48b8347acd7177eaa
Kumaran <kumaran101088@gmail.com>
2022-09-03 16:38:09 +0530

    31th commit

--- sample_app/myapp/views.py
+++ sample_app/myapp/views.py
@@ -16,21 +16,8 @@ def home(request):
 def member_view(request, id):
     try:
         ind_member = Member.objects.filter(member_id=id)[0]
-    #     ind_bmi = round((ind_member.weight/(ind_member.height*ind_member.height)) * 10000)
-    #     ind_medication = 1 if ind_member.medication == 'Y' else 0
-    #     ind_bp = round((ind_member.st_bp+ind_member.dy_bp)/2)
-    #     individual_score = (ind_bmi*10) + (ind_member.smoke*10) + (ind_medication*10) + (ind_member.alco*10) + (ind_member.age*10) + (ind_member.active*10) + (ind_member.cardio*20) + (ind_bp*10) + (ind_member.gluc*10)
     except:
-        pass
-    # members = Member.objects.all()
-    # individual_scores, percentile_scores = [], []
-    # for member in members:
-    #     bmi = round((member.weight/(member.height*member.height)) * 10000)
-    #     medication = 1 if member.medication == 'Y' else 0
-    #     bp = round((member.st_bp+member.dy_bp)/2)
-    #     score = (bmi*10) + (member.smoke*10) + (medication*10) + (member.alco*10) + (member.age*10) + (member.active*10) + (member.cardio*20) + (bp*10) + (member.gluc*10)
-    #     individual_scores.append(score)
-    # risk_score = round((len(list(filter(lambda x : x < individual_score, individual_scores)))/len(individual_scores)) * 100)
+        return render(request, 'html_files/not_found.html')
     return render(request, 'html_files/member.html', {'member':ind_member})
 
 @api_view(['GET'])
