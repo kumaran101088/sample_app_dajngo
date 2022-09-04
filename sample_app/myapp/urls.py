91c515da10aaa2c7fb02edcf6c8f41b1472b3a0b
Kumaran <kumaran101088@gmail.com>
2022-09-04 17:27:52 +0530

    3th commit

--- sample_app/myapp/urls.py
+++ sample_app/myapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import home, convert, member_view, total_risk_score, individual_risk_score
+from .views import home, convert, member_view, total_risk_score, individual_risk_score, search
 
 urlpatterns = [
 
     #TEMPLATES
     path('home/', home, name='home'),
     path('add/', convert, name='add'),
+    path('search/', search, name='search'),
     path('member/<int:id>', member_view, name='member_view'),
 
     #APIs
